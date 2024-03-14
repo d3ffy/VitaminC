f6ca02e3f4d15c12ea004ca2be39871247603238
d3ffy <tv4570@gmail.com>
2024-03-14 22:04:19 +0700

    update code for ESP and UNO

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -2,38 +2,51 @@
 const byte BUFFER_SIZE = 16;
 char receivedMessage[BUFFER_SIZE];
 byte messageIndex = 0;
+const byte LED = 13;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   messageHandler();
   functionHandler();
+  clearMessage();
   delay(1000);
+  digitalWrite(LED, LOW);
 }
 
 void messageHandler() {
-  while (Serial.available()) {
+  while (Serial.available() > 0) {
     char receivedChar = Serial.read();
-    if (receivedChar == '\n' || messageIndex >= BUFFER_SIZE - 1) {
-      receivedMessage[messageIndex] = '\0';
+    if (receivedChar == '\n') {
       messageIndex = 0;
+      break;
     } else {
       receivedMessage[messageIndex++] = receivedChar;
     }
+    delay(100);
   }
 }
 
 void functionHandler() {
+  // use strncmp for handling future complicate function
   if (strncmp(receivedMessage, "RED", 3) == 0) {
     Serial.println("Blink red color");
+    digitalWrite(LED, HIGH);
   } else if (strncmp(receivedMessage, "GREEN", 5) == 0) {
     Serial.println("Blink green color");
   } else if (strncmp(receivedMessage, "BLUE", 4) == 0) {
     Serial.println("Blink blue color");
-  } else {
+  } else if(receivedMessage != NULL){
     Serial.print("Received: ");
     Serial.println(receivedMessage);
+  }
+}
+
+void clearMessage() {
+  for (uint8_t i = 0; i < BUFFER_SIZE; i++) {
+  	receivedMessage[i] = '\0';
   }
 }
--- uno/uno.ino
+++ uno/uno.ino
@@ -1,9 +1,17 @@
+// Arduino
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println("Hello, ESP8266!");
-  delay(1000);
-  Serial.println("RED");
+  while (Serial.availableForWrite()) {
+    delay(2000);
+    Serial.println("Hello!");
+    delay(2000);
+    Serial.println("RED");
+    delay(2000);
+    Serial.println("GREEN");
+    delay(2000);
+    Serial.println("BLUE");
+  }
 }
