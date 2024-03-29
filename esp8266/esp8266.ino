7fd4ffcc1121b39b633761850d05a2022a5f48df
d3ffy <tv4570@gmail.com>
2024-03-29 20:39:52 +0700

    esp8266 + uno update

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -1,93 +1,80 @@
 #include <ESP8266WiFi.h>
 #include <Firebase_ESP_Client.h>
 
-// Define WiFi Component
-#define WIFI_SSID "deffy's room"
-#define WIFI_PASS "FreeWifi"
+// WiFi Credentials as constants
+const char* WIFI_SSID = "deffy's room";
+const char* WIFI_PASS = "FreeWifi";
 
-// Define Buffer Size
-const byte BUFFER_SIZE = 16;
-char receivedMessage[BUFFER_SIZE];
-byte messageIndex = 0;
+// Firebase Credentials and Configuration as constants
+const char* API_KEY = "AIzaSyCPkTRiFpWFcjuJvAiOZCqoMXJN2Gvtzjc";
+const char* DATABASE_URL = "https://vitaminc-4695a-default-rtdb.asia-southeast1.firebasedatabase.app/";
+const char* USER_EMAIL = "12345@gmail.com";
+const char* USER_PASSWORD = "12345";
 
-// Define Firebase Component
-#define API_KEY "AIzaSyCPkTRiFpWFcjuJvAiOZCqoMXJN2Gvtzjc"
-#define DATABASE_URL "https://vitaminc-4695a-default-rtdb.asia-southeast1.firebasedatabase.app/"
-//Provide the token generation process info.
-#include "addons/TokenHelper.h"
-//Provide the RTDB payload printing info and other helper functions.
-#include "addons/RTDBHelper.h"
+// Define Pin Number as a constant
+const byte redLED = 13;
 
-// Define Firebase Data object
+// Firebase objects as global to initialize once
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
-// Define Pin Number
-const byte LED = 13;
-
 void setup() {
   Serial.begin(9600);
-  pinMode(LED, OUTPUT);
+  pinMode(redLED, OUTPUT);
+  connectWiFi(); // Initial WiFi Connect
+  
+  // Firebase Setup
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 }
 
 void loop() {
-  if (WiFi.status() != WL_CONNECTED) {
-    connectWiFi();
-  } else {
-    messageHandler();
-    functionHandler();
-    clearMessage();
+  static String receivedMessage; // Use static to keep it between calls but avoid global scope
+
+  while (WiFi.status() == WL_CONNECTED) {
+    messageHandler(receivedMessage);
+    functionHandler(receivedMessage);
+    receivedMessage = ""; // Clear the message
     delay(1000);
-    digitalWrite(LED, LOW);  
+  }
+  connectWiFi();
+}
+
+void messageHandler(String& receivedMessage) {
+  if (Serial.available() > 0) {
+    receivedMessage = Serial.readStringUntil('\n');
   }
 }
 
-void messageHandler() {
-  while (Serial.available() > 0) {
-    char receivedChar = Serial.read();
-    if (receivedChar == '\n') {
-      messageIndex = 0;
-      break;
-    } else {
-      receivedMessage[messageIndex++] = receivedChar;
-    }
-    delay(100);
-  }
-}
-
-void functionHandler() {
-  // use strncmp for handling future complicate function
-  if (strncmp(receivedMessage, "RED", 3) == 0) {
-    Serial.println("Blink red color");
-    digitalWrite(LED, HIGH);
-  } else if (strncmp(receivedMessage, "GREEN", 5) == 0) {
-    Serial.println("Blink green color");
-  } else if (strncmp(receivedMessage, "BLUE", 4) == 0) {
-    Serial.println("Blink blue color");
-  } else if(receivedMessage != NULL){
-    Serial.print("Received: ");
+void functionHandler(const String& receivedMessage) {
+  if (receivedMessage.startsWith("RED")) {
+    Serial.println(F("Blink red color"));
+    digitalWrite(redLED, HIGH);
+  } else if (receivedMessage.startsWith("GREEN")) {
+    Serial.println(F("Blink green color"));
+  } else if (receivedMessage.startsWith("BLUE")) {
+    Serial.println(F("Blink blue color"));
+  } else if (receivedMessage.length() > 0) {
+    Serial.print(F("Received: "));
     Serial.println(receivedMessage);
   }
 }
 
-void clearMessage() {
-  for (uint8_t i = 0; i < BUFFER_SIZE; i++) {
-  	receivedMessage[i] = '\0';
-  }
-}
-
 void connectWiFi() {
-  WiFi.mode(WIFI_STA); // Set ESP to Station Mode
-  WiFi.begin(WIFI_SSID, WIFI_PASS); // Connect to WiFi
-  Serial.print("Connecting to WiFi");
-
-  // loop until ESP connected to WiFi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.print(F("Connecting to WiFi"));
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
     delay(1000);
   }
   Serial.println();
-  Serial.print("Connected to IP Address: ");
-  Serial.println(WiFi.localIP()); // Annouce Local IP
+  Serial.print(F("Connected to IP Address: "));
+  Serial.println(WiFi.localIP());
 }
--- uno/uno.ino
+++ uno/uno.ino
@@ -1,17 +1,88 @@
 // Arduino
+const int blueLED = 11;
+const int greenLED = 12;
+const int redLED = 13;
+
+const int whiteLED = 2;
+const int S0 = 3;
+const int S1 = 4;
+const int out = 5;
+const int S2 = 6;
+const int S3 = 7;
+
+// Define int variables
+int redValue = 0;
+int greenValue = 0;
+int blueValue = 0;
+int frequency = 0;
+
 void setup() {
+  pinMode(redLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  pinMode(blueLED, OUTPUT);
+
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(out, INPUT);
+  pinMode(whiteLED, OUTPUT);
+  
+  digitalWrite(whiteLED, LOW);
+  digitalWrite(S0, HIGH);
   Serial.begin(9600);
 }
 
 void loop() {
   while (Serial.availableForWrite()) {
-    delay(2000);
-    Serial.println("Hello!");
-    delay(2000);
-    Serial.println("RED");
-    delay(2000);
-    Serial.println("GREEN");
-    delay(2000);
-    Serial.println("BLUE");
+
+    getColorValue();
+    Serial.print("Red Freq = ");
+    Serial.print(redValue); 
+    Serial.print("   ");
+    Serial.print("Green Freq = ");
+    Serial.print(greenValue); 
+    Serial.print("   ");
+    Serial.print("Blue Freq = ");
+    Serial.println(blueValue); 
   }
+}
+
+int getRed() {
+  digitalWrite(redLED, HIGH);
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,LOW);
+  delay(1000);
+  frequency = pulseIn(out, LOW); // Get the Red Color Frequency
+  digitalWrite(redLED, LOW);
+  return frequency;
+}
+
+int getGreen() {
+  digitalWrite(greenLED, HIGH);
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,HIGH);
+  delay(1000);
+  frequency = pulseIn(out, LOW); // Get the Green Color Frequency
+  digitalWrite(greenLED, LOW);
+  return frequency;
+}
+
+int getBlue() {
+  digitalWrite(blueLED, HIGH);
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,HIGH);
+  delay(1000);
+  frequency = pulseIn(out, LOW); // Get the Blue Color Frequency
+  digitalWrite(blueLED, LOW);
+  return frequency;
+}
+
+void getColorValue() {
+  redValue = getRed();
+  delay(500); 
+  greenValue = getGreen();
+  delay(500); 
+  blueValue = getBlue();
+  delay(500); 
 }
